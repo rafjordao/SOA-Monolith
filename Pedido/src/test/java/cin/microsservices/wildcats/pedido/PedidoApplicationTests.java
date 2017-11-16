49266ce533c39d3b53eb3fd2cd28bc7b5f87c8e9
augusto94 <arl4@cin.ufpe.br>
2017-11-15 21:30:50 -0300

    Mergiando com branch Augusto

--- Pedido/src/test/java/cin/microsservices/wildcats/pedido/PedidoApplicationTests.java
+++ Pedido/src/test/java/cin/microsservices/wildcats/pedido/PedidoApplicationTests.java
@@ -1,5 +1,9 @@
 package cin.microsservices.wildcats.pedido;
 
+import cin.microsservices.wildcats.pedido.domain.pedido.Pedido;
+import cin.microsservices.wildcats.pedido.domain.pedido.StatusPedido;
+import cin.microsservices.wildcats.pedido.dto.pedido.ItemPedidoDTO;
+import cin.microsservices.wildcats.pedido.rest.PedidoRestService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,27 +12,49 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import cin.microsservices.wildcats.pedido.domain.pedido.ItemPedido;
 
+import java.util.List;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PedidoApplicationTests {
+	private ItemPedidoDTO item1DTO = new ItemPedidoDTO();
 	private ItemPedido item1 = new ItemPedido();
-	
+	private PedidoRestService pedido_rest = new PedidoRestService();
+
 	@Before
 	public void setUp() {
 		//Criação de item pedido
-		
-		item1.setIdProduto(36);
-		item1.setQuantidade(65);
+
+		item1.setIdProduto(1);
+		item1.setQuantidade(2);
+		item1DTO.setIdPedido(1);
+		item1DTO.setIdCliente(1);
+		item1DTO.setItem(item1);
 	}
-	
 	/**
 	 * Teste simples na criação de um ItemPedido
 	 * 12/11/2017
 	 */
 	@Test
 	public void testCriacaoDePedido() {
+		pedido_rest.adicionaItemPedido(item1DTO);
+		List<Pedido> lista_pedidos;
+		lista_pedidos = pedido_rest.buscarPedidosPorCliente(Long.valueOf(1));
 		//=================VERIFICAÇÃO===============
-		
+		assertTrue(lista_pedidos.size() > 0);
+	}
+	@Test
+	public void testRemovePedido() {
+		pedido_rest.removeItemPedido(item1DTO);
+		List<Pedido> lista_pedidos;
+		lista_pedidos = pedido_rest.buscarPedidosPorCliente(Long.valueOf(1));
+		//=================VERIFICAÇÃO===============
+		assertFalse(lista_pedidos.size() > 0);
 	}
 
 }
